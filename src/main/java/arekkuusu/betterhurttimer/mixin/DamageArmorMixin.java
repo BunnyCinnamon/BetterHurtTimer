02124a45111cb7067d5df2ec34ca8d254d9fa438
ArekkuusuJerii <edgargcx99@gmail.com>
2020-08-13 11:50:18 -0500

    Fix Bukkit Crash

--- src/main/java/arekkuusu/betterhurttimer/mixin/DamageArmorMixin.java
+++ src/main/java/arekkuusu/betterhurttimer/mixin/DamageArmorMixin.java
@@ -9,14 +9,13 @@ import net.minecraftforge.common.util.LazyOptional;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class DamageArmorMixin {
 
-    @Redirect(method = "attackEntityFrom(Lnet/minecraft/util/DamageSource;F)Z", at = @At(target = "Lnet/minecraft/entity/LivingEntity;damageShield(F)V", value = "INVOKE"))
+    //Forge Compliant
+    @Redirect(method = "attackEntityFrom(Lnet/minecraft/util/DamageSource;F)Z", at = @At(target = "Lnet/minecraft/entity/LivingEntity;damageShield(F)V", value = "INVOKE"), require = 0)
     public void damageShield(LivingEntity entity, float damage) {
         LazyOptional<HurtCapability> optional = Capabilities.hurt(entity);
         if (optional.isPresent()) {
@@ -36,7 +35,7 @@ public abstract class DamageArmorMixin {
         }
     }
 
-    @Redirect(method = "applyArmorCalculations(Lnet/minecraft/util/DamageSource;F)F", at = @At(target = "Lnet/minecraft/entity/LivingEntity;func_230294_b_(Lnet/minecraft/util/DamageSource;F)V", value = "INVOKE"))
+    @Redirect(method = "applyArmorCalculations(Lnet/minecraft/util/DamageSource;F)F", at = @At(target = "Lnet/minecraft/entity/LivingEntity;func_230294_b_(Lnet/minecraft/util/DamageSource;F)V", value = "INVOKE"), require = 0)
     public void damageArmor(LivingEntity entity, DamageSource source, float damage) {
         LazyOptional<HurtCapability> optional = Capabilities.hurt(entity);
         if (optional.isPresent()) {
@@ -55,6 +54,49 @@ public abstract class DamageArmorMixin {
             func_230294_b_(source, damage);
         }
     }
+    //Forge Compliant
+
+    //Spigot Compliant
+    @Redirect(method = "damageEntity_CB(Lnet/minecraft/util/DamageSource;F)Z", at = @At(target = "Lnet/minecraft/entity/LivingEntity;damageShield(F)V", value = "INVOKE"), require = 0)
+    public void damageShieldS(LivingEntity entity, float damage) {
+        LazyOptional<HurtCapability> optional = Capabilities.hurt(entity);
+        if (optional.isPresent()) {
+            HurtCapability capability = optional.orElseThrow(UnsupportedOperationException::new);
+            if (capability.ticksToShieldDamage > 0) {
+                if (Double.compare(Math.max(0, capability.lastShieldDamage + BHTConfig.Runtime.DamageFrames.nextAttackDamageDifference), damage) < 0) {
+                    damageShield((float) (damage - capability.lastShieldDamage));
+                    capability.lastShieldDamage = damage;
+                }
+            } else {
+                damageShield(damage);
+                capability.lastShieldDamage = damage;
+                capability.ticksToShieldDamage = BHTConfig.Runtime.DamageFrames.shieldResistantTime;
+            }
+        } else {
+            damageShield(damage);
+        }
+    }
+
+    @Redirect(method = "damageEntity_CB(Lnet/minecraft/util/DamageSource;F)F", at = @At(target = "Lnet/minecraft/entity/LivingEntity;func_230294_b_(Lnet/minecraft/util/DamageSource;F)V", value = "INVOKE"), require = 0)
+    public void damageArmorS(LivingEntity entity, DamageSource source, float damage) {
+        LazyOptional<HurtCapability> optional = Capabilities.hurt(entity);
+        if (optional.isPresent()) {
+            HurtCapability capability = optional.orElseThrow(UnsupportedOperationException::new);
+            if (capability.ticksToArmorDamage > 0) {
+                if (Double.compare(Math.max(0, capability.lastArmorDamage + BHTConfig.Runtime.DamageFrames.nextAttackDamageDifference), damage) < 0) {
+                    func_230294_b_(source, (float) (damage - capability.lastArmorDamage));
+                    capability.lastArmorDamage = damage;
+                }
+            } else {
+                func_230294_b_(source, damage);
+                capability.lastArmorDamage = damage;
+                capability.ticksToArmorDamage = BHTConfig.Runtime.DamageFrames.armorResistantTime;
+            }
+        } else {
+            func_230294_b_(source, damage);
+        }
+    }
+    //Spigot Compliant
 
     @Shadow
     protected abstract void func_230294_b_(DamageSource p_230294_1_, float p_230294_2_);
