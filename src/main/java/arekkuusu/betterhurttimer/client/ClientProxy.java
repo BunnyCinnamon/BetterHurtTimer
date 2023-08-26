1b3c7b27e444ec16c114b481ec514707f21aad24
ArekkuusuJerii <edgargcx99@gmail.com>
2023-08-26 01:34:23 -0600

    Fix animations

--- src/main/java/arekkuusu/betterhurttimer/client/ClientProxy.java
+++ src/main/java/arekkuusu/betterhurttimer/client/ClientProxy.java
@@ -1,22 +1,9 @@
 package arekkuusu.betterhurttimer.client;
 
 import arekkuusu.betterhurttimer.common.proxy.IProxy;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientProxy implements IProxy {
-    // holi :)
-    // a que hora sales por el pan? :3
-    public static int preHurtRender;
-
-    @Override
-    public void setPreHurtTime(LivingEntity entity) {
-        if(entity == Minecraft.getInstance().cameraEntity) {
-            ClientProxy.preHurtRender = entity.hurtTime;
-        }
-    }
 }
--- src/main/java/arekkuusu/betterhurttimer/mixin/HurtAnimationMixin.java
+++ src/main/java/arekkuusu/betterhurttimer/mixin/HurtAnimationMixin.java
@@ -2,21 +2,7 @@ package arekkuusu.betterhurttimer.mixin;
 
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public class HurtAnimationMixin {
-
-    @Shadow
-    public int hurtTime;
-
-    @Inject(method = "animateHurt", at = @At("HEAD"), cancellable = true)
-    public void performHurtAnimation(CallbackInfo info) {
-        if (this.hurtTime > 0) {
-            info.cancel();
-        }
-    }
 }
